2e58298c3f2dbf56a0a09f2602b4fd3010e7bcf4
Devang Gaur <dgaur@redhat.com>
2018-02-09 16:40:31 +0530

    Modified tests

--- performance/scripts/hypothesis-scout/_setenv.sh
+++ performance/scripts/hypothesis-scout/_setenv.sh
@@ -10,7 +10,7 @@ export BUILD_NUMBER=0
 export SERVER_SCHEME=http
 
 # supervisor server machine address.
-export SERVER_HOST=hdd-supervisor-test1.192.168.42.17.nip.io
+export SERVER_HOST=http://measurement-fabric8-hypothesis-measurement-api.dev.rdu2c.fabric8.io/measurement
 
 # Locust SSH user
 export SSH_USER=centos
@@ -19,13 +19,13 @@ export SSH_USER=centos
 export SSH_WORKDIR=/home/centos
 
 # Locust MASTER node.
-#export MASTER_HOST=osioperf-server-2
+export MASTER_HOST=osioperf-server-2
 
 # A number of Locust slaves to use.
-#export SLAVES=1
+export SLAVES=1
 
 # A prefix for a Locust slave node address.
-#export SLAVE_PREFIX=osioperf-client-
+export SLAVE_PREFIX=osioperf-client-
 
 # A number of users to spawn.
 export USERS=1
@@ -34,7 +34,7 @@ export USERS=1
 export USER_HATCH_RATE=1
 
 # A name of the property file with username=password list of users to be logged in
-#export USERS_PROPERTIES_FILE=osioperftest.users.properties
+export USERS_PROPERTIES_FILE=osioperftest.users.properties
 
 # A file where USER_TOKENS environmental variable is supposed to be set.
 # This file is copied to locust master/slaves to be sources by BASH before executing the locust itself.
@@ -45,13 +45,13 @@ export ENV_FILE=/tmp/osioperftest.users.env
 export ZABBIX_REPORT_ENABLED=false
 
 # An address of Zabbix server
-#export ZABBIX_SERVER=zabbix.devshift.net
+export ZABBIX_SERVER=zabbix.devshift.net
 
 # A port of Zabbix server used by zabbix_sender utility
-#export ZABBIX_PORT=10051
+export ZABBIX_PORT=10051
 
 # A hostname in Zabbix the report is for
-#export ZABBIX_HOST=auth.prod-preview
+export ZABBIX_HOST=hdd-measurement-api.dev
 
 # A number of seconds for how long the test should run
 export DURATION=300
--- performance/scripts/hypothesis-scout/hypothesis-scout.py
+++ performance/scripts/hypothesis-scout/hypothesis-scout.py
@@ -3,37 +3,27 @@ from locust import HttpLocust, TaskSet, task
 serverScheme = "@@SERVER_SCHEME@@"
 serverHost = "@@SERVER_HOST@@"
 
-med_payload1 = {
-	"id": "0001",
-	"type": "donut",
-	"name": "Cake",
-	"ppu": 0.55,
-	"batters":
-		{
-			"batter":
-				[
-					{ "id": "1001", "type": "Regular" },
-					{ "id": "1002", "type": "Chocolate" },
-					{ "id": "1003", "type": "Blueberry" },
-					{ "id": "1004", "type": "Devil's Food" }
-				]
+heavy_payload1 = {
+		"scout_id" : "a4e1b0cf-2a08-4297-83f3-4db896d7e0fb",
+		"data" : [{"date":"1/1/2", "value":1234}],
+		"source": {
+					"date": "2018-02-12 12:05:45:999 pm"
 		},
-	"topping":
-		[
-			{ "id": "5001", "type": "None" },
-			{ "id": "5002", "type": "Glazed" },
-			{ "id": "5005", "type": "Sugar" },
-			{ "id": "5007", "type": "Powdered Sugar" },
-			{ "id": "5006", "type": "Chocolate with Sprinkles" },
-			{ "id": "5003", "type": "Chocolate" },
-			{ "id": "5004", "type": "Maple" }
-		]
-}
+		"descriptor": {
+			"id": 1234,
+			"unit_of_measure": "seconds"
+		},
+		"dimensions": {
+			"node": 3,
+			"pod": 1234,
+			"container": 223434
+		}
+	}
 
 class ScoutTaskSet(TaskSet):
 
     def __pushmetric(self, payload, name):
-        response = self.client.post("/api/v1.0/stats", payload, name=name ,catch_response=True)
+        response = self.client.post("/api/v1.0/measurements", payload, name=name ,catch_response=True)
     	content = response.content
 
     	try:
@@ -42,19 +32,16 @@ class ScoutTaskSet(TaskSet):
 			else:
 				response.success()
     	except ValueError:
-			response.failure("Got wrong response EXCEEEEPTIONN!: [" + content + "]")
+			response.failure("Got wrong response EXCEPTIONN!: [" + content + "]")
 
-    @task(3)
+    @task(10)
     def pushmetric_light_payload(self):
 		self.__pushmetric({ "hello": "world" }, "pushmetric__light_payload")
 
-    @task(3)
-    def pushmetric_med_payload(self):
-        self.__pushmetric(med_payload1, "pushmetric_med_payload")
-
-    @task(3)
+    @task(10)
     def pushmetric_heavy_payload(self):
-        self.__pushmetric({ "hello": "world" }, name="pushmetric_heavy_payload")
+        self.__pushmetric(heavy_payload1, "pushmetric_heavy_payload")
+
 
 class MyLocust(HttpLocust):
     task_set = ScoutTaskSet
