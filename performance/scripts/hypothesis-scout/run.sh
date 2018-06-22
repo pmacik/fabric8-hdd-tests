87d66f3a989f68a0e80ab43366793d214b96df05
Pavel Macík <pavel.macik@gmail.com>
2018-06-22 15:55:41 +0200

    Update commons.

--- performance/scripts/hypothesis-scout/run.sh
+++ performance/scripts/hypothesis-scout/run.sh
@@ -3,7 +3,7 @@
 source _setenv.sh
 
 export COMMON="common.git"
-git clone https://github.com/pmacik/openshiftio-performance-common $COMMON
+git clone -b pre-metrics https://github.com/pmacik/openshiftio-performance-common $COMMON
 
 echo " Wait for the server to become available"
 ./_wait-for-server.sh
